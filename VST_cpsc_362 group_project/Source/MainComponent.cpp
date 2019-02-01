09209351616c3234413c0a091f4eb669511ded9e
Robert Rivas <robrrivas@gmail.com>
2019-01-31 23:20:58 -0800

    Implemented hello World
    Testing compilation in juice and xcode from latest pull

--- VST_cpsc_362 group_project/Source/MainComponent.cpp
+++ VST_cpsc_362 group_project/Source/MainComponent.cpp
@@ -63,6 +63,11 @@ void MainComponent::paint (Graphics& g)
     g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
 
     // You can add your drawing code here!
+    g.setFont(Font(16.0F));
+    g.setColour(Colours::white);
+    g.drawText("Hello World !", getLocalBounds(), Justification::centred, true );
+    
+    
 }
 
 void MainComponent::resized()
