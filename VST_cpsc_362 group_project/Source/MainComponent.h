67b45d6de03897136ec0be0099ac64a238e839c5
Robert Rivas <robrrivas@gmail.com>
2019-02-13 08:32:52 -0800

    more comments explaining juce classes
    added comments on using juce classes

--- VST_cpsc_362 group_project/Source/MainComponent.h
+++ VST_cpsc_362 group_project/Source/MainComponent.h
@@ -8,7 +8,7 @@
 
 #pragma once
 
-#include "../JuceLibraryCode/JuceHeader.h"
+#include "../JuceLibraryCode/JuceHeader.h" //////this is where juce includes all of its headersfor its modules. the descriptions are in online documentation to use you can initialize down below in private part of main component class like how i did for this one (dsp::Oscillator<float> wave2;) test it out explore other classes for anything useful.
 #include "waveForms.h"
 
 
