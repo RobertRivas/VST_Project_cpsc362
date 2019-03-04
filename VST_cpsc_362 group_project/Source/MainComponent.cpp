7036e59bd881b85c3f9e75fb70a9ca0654ae40cd
bobberino98 <bobberino98@gmail.com>
2019-03-04 11:18:47 -0800

    added ladder filter and controls for filter

--- VST_cpsc_362 group_project/Source/MainComponent.cpp
+++ VST_cpsc_362 group_project/Source/MainComponent.cpp
@@ -11,7 +11,40 @@
 
 MainComponent::MainComponent()
 {
-    // Make sure you set the size of the component after
+	dial1.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag); //configure sliders' properties
+	dial1.setTextBoxStyle(Slider::TextBoxBelow, false, 120, dial1.getTextBoxHeight());
+	dial1.setRange(0, 1);
+	dial1.setValue(0.5);
+	dial1Label.setText("Resonance", dontSendNotification);
+	dial1Label.attachToComponent(&dial1, false);
+	dial1.addListener(this);				//use listener class to detect our changes to the slider values
+	addAndMakeVisible(dial1);
+	addAndMakeVisible(dial1Label);									//Make sliders & labels visible
+
+	dial2.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag);
+	dial2.setTextBoxStyle(Slider::TextBoxBelow, false, 120, dial2.getTextBoxHeight());
+	dial2.setRange(1, 100);
+	dial2.setValue(50);
+	dial2Label.setText("Drive", dontSendNotification);
+	dial2Label.attachToComponent(&dial2, false);
+	dial2.addListener(this);
+	addAndMakeVisible(dial2);
+	addAndMakeVisible(dial2Label);
+
+	frequencySlider.setRange(20, 12000);
+	frequencySlider.setValue(6000);
+	frequencySlider.setSkewFactorFromMidPoint(6000.0);
+	frequencySlider.setTextBoxStyle(Slider::TextBoxLeft, false, 120, frequencySlider.getTextBoxHeight());
+	frequencySlider.setTextValueSuffix(" Hz");					//example slider value suffix
+	frequencyLabel.setText("Frequency", dontSendNotification); // This is just an example label
+	frequencyLabel.attachToComponent(&frequencySlider, true);
+	frequencySlider.addListener(this);
+	addAndMakeVisible(frequencyLabel);
+	addAndMakeVisible(frequencySlider);
+
+	
+	
+	// Make sure you set the size of the component after
     // you add any child components.
     setSize (800, 600);
 	
@@ -29,10 +62,14 @@ MainComponent::~MainComponent()
 //==============================================================================
 void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
 {
+	dsp::ProcessSpec spec;
+	spec.sampleRate = sampleRate;
     // This function will be called when the audio device is started, or when
     // its settings (i.e. sample rate, block size, etc) are changed.
-	IIRCoefficients temp = IIRCoefficients::makeLowPass(sampleRate, 10000, 5);
-	lp1.setCoefficients(temp);
+	
+	lp1.prepare(spec);
+	rv6.prepare(spec);
+
     // You can use this function to initialise any resources you might need,
     // but be careful - it will be called on the audio thread, not the GUI thread.
 
@@ -41,22 +78,23 @@ void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRat
 
 void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
 {
-	
+	lp1.setMode(dsp::LadderFilter<float>::Mode::LPF12);
     float* leftSpeaker = bufferToFill.buffer->getWritePointer(0, bufferToFill.startSample);//start of buffer fill
     float* rightSpeaker = bufferToFill.buffer->getWritePointer(1, bufferToFill.startSample);
-    
+	float* channels[] = { leftSpeaker, rightSpeaker };
     for (int sample = 0; sample < bufferToFill.buffer->getNumSamples(); ++sample){
         double theWave = wave.SawWave(sample, 440, 0.25); ////input to saw wave function might be a good start to apply MIDI
 		
-        leftSpeaker[sample] = rightSpeaker[sample] = theWave;
-		
+		leftSpeaker[sample] = rightSpeaker[sample] = theWave;
         
     }
     // Your audio-processing code goes here!
-
+	dsp::AudioBlock<float> ab1 = dsp::AudioBlock<float>::AudioBlock(channels, 2, bufferToFill.buffer->getNumSamples());
+	dsp::ProcessContextReplacing<float> pc = dsp::ProcessContextReplacing<float>::ProcessContextReplacing(ab1);
     // For more details, see the help for AudioProcessor::getNextAudioBlock()
-	lp1.processSamples(leftSpeaker, bufferToFill.buffer->getNumSamples());
-	lp1.processSamples(rightSpeaker, bufferToFill.buffer->getNumSamples());
+	lp1.process(pc);
+	rv6.process(pc);
+	
     // Right now we are not producing any data, in which case we need to clear the buffer
     // (to prevent the output of random noise)
     //bufferToFill.clearActiveBufferRegion(); ///had to comment this out it was killing output
@@ -82,6 +120,16 @@ void MainComponent::paint (Graphics& g)
 
 void MainComponent::resized()
 {
+	//=========================Slider Sizing and positioning============================
+	Rectangle<int> area = getLocalBounds();	//This rectangles are used to map sliders on screen
+	Rectangle<int> dialArea = area.removeFromBottom(area.getHeight() / 2); //dialArea targets the bottom of screen
+
+	frequencySlider.setBounds(100, 40, getWidth() - 130, 20);
+	dial1.setBounds(dialArea.removeFromLeft(area.getWidth() / 2)); //Set position to bottom left of screen
+	dial2.setBounds(dialArea.removeFromRight(area.getWidth() / 2)); //Set position to bottom right of screen
+																	//=================================================================================
+
+
     // This is called when the MainContentComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
--- VST_cpsc_362 group_project/Source/MainComponent.h
+++ VST_cpsc_362 group_project/Source/MainComponent.h
@@ -11,14 +11,12 @@
 #include "../JuceLibraryCode/JuceHeader.h" //////this is where juce includes all of its headersfor its modules. the descriptions are in online documentation to use you can initialize down below in private part of main component class like how i did for this one (dsp::Oscillator<float> wave2;) test it out explore other classes for anything useful.
 #include "waveForms.h"
 
-
-
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent   : public AudioAppComponent
+class MainComponent   : public AudioAppComponent, public Slider::Listener
 {
 public:
     //==============================================================================
@@ -29,16 +27,39 @@ public:
     void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
     void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
+	
+	void sliderValueChanged(Slider * slider) {
+		if (slider == &frequencySlider) {
+			double frequency = slider->getValue();
+			//Add behavior for when we make changed to the sliders
+			//since this is a frequency slider maybe update the frequency like
+			lp1.setCutoffFrequencyHz(frequency);
+		}
+		else if (slider == &dial1) {
+			//add behavior here
+			double res = slider->getValue();
 
+			lp1.setResonance(res);
+		}
+		else if (slider == &dial2) {
+			//add behavior here
+			double drv = slider->getValue();
+
+			lp1.setDrive(drv);
+		}
+	}
     //==============================================================================
     void paint (Graphics& g) override;
     void resized() override;
 
 private:
     waveForms wave; ///call to wave form class in waveforms.h
-    
+	
     dsp::Oscillator<float> wave2; //created this instance to get the ball rolling on calling juce classes from documentation
-	IIRFilter lp1;
+	dsp::LadderFilter<float> lp1;
+	dsp::Reverb rv6;
+	
+
 	
     //i had to manually add this dsp module from the modules section to the left on the projucer here is documentation https://docs.juce.com/master/juce__Oscillator_8h.html
     
@@ -47,6 +68,16 @@ private:
 
 	
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+		//=======================members used for gui implementation===========================
+		bool loading;//flag variable we can use to determine whether we're starting with new audio settings or loading from saved presets
+	Slider dial1;
+	Slider dial2;
+	Slider frequencySlider;
+	Label frequencyLabel;
+	Label dial1Label;
+	Label dial2Label;
+	Label durationLabel;
+	//=====================================================================================
 };
 
 
