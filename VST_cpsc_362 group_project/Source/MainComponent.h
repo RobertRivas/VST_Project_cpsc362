ed2ebbe12dda2fbb31bac5202487f30628cb3e22
Alexander Feldman <35717802+Feldman0007@users.noreply.github.com>
2019-03-24 20:55:59 -0700

    Update MainComponent.h

--- VST_cpsc_362 group_project/Source/MainComponent.h
+++ VST_cpsc_362 group_project/Source/MainComponent.h
@@ -8,18 +8,26 @@
 
 #pragma once
 
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#pragma once
+
 #include "../JuceLibraryCode/JuceHeader.h" //////this is where juce includes all of its headersfor its modules. the descriptions are in online documentation to use you can initialize down below in private part of main component class like how i did for this one (dsp::Oscillator<float> wave2;) test it out explore other classes for anything useful.
 #include "waveForms.h"
-
-
 
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-
-class MainComponent   : public AudioAppComponent, public Slider::Listener
+class MainComponent   : public AudioAppComponent, public Slider::Listener, public Button::Listener, private MidiInputCallback,
+private MidiKeyboardStateListener
 {
 public:
     //==============================================================================
@@ -31,30 +39,157 @@ public:
     void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
 	
-	void sliderValueChanged(Slider * slider) {
-		if (slider == &frequencySlider) {
+	void sliderValueChanged(Slider * slider)  override{
+		if (slider == &cutoffFrequencySlider) {
 			double frequency = slider->getValue();
-			//Add behavior for when we make changed to the sliders
-			//since this is a frequency slider maybe update the frequency like
-			wave.setFreq(frequency);
+			lp1.setCutoffFrequencyHz(frequency);
 		}
-		else if (slider == &dial1) {
-			//add behavior here
+		else if (slider == &resonanceSlider) {
+			double res = slider->getValue();
+
+			lp1.setResonance(res);
 		}
-		else if (slider == &dial2) {
-			//add behavior here
+		else if (slider == &driveSlider) {
+			double drv = slider->getValue();
+
+			lp1.setDrive(drv);
 		}
 	}
-
+	void buttonClicked(Button * button) override {
+		if (button == &filterButton) 
+		{
+			if (!filterType)
+			{
+				filterButton.setButtonText("high pass");
+				filterType = true;
+				//add code here to actually change the filter being used
+			}
+			else 
+			{
+				filterButton.setButtonText("low pass");
+				filterType = false;
+				//add code here to actually change the filter being used
+			}
+		}
+	}
+	
+    void setMidiInput (int index);
+    void handleIncomingMidiMessage (MidiInput* source, const MidiMessage& message) override;
+    
+    void handleNoteOn (MidiKeyboardState*, int midiChannel, int midiNoteNumber, float velocity) override;
+   
+    
+    void handleNoteOff (MidiKeyboardState*, int midiChannel, int midiNoteNumber, float /*velocity*/) override;
+    
+    
+    
+    class IncomingMessageCallback   : public CallbackMessage
+    {
+    public:
+        IncomingMessageCallback (MainComponent* o, const MidiMessage& m, const String& s)
+        : owner (o), message (m), source (s)
+        {}
+        
+        void messageCallback() override
+        {
+            if (owner != nullptr)
+                owner->addMessageToList (message, source);
+        }
+        
+        AudioAppComponent::SafePointer<MainComponent> owner;
+        MidiMessage message;
+        String source;
+    };
+    void postMessageToList (const MidiMessage& message, const String& source)
+    {
+        (new IncomingMessageCallback (this, message, source))->post();
+    }
+    
+    void addMessageToList (const MidiMessage& message, const String& source)
+    {
+        auto time = message.getTimeStamp() - startTime;
+        
+        auto hours   = ((int) (time / 3600.0)) % 24;
+        auto minutes = ((int) (time / 60.0)) % 60;
+        auto seconds = ((int) time) % 60;
+        auto millis  = ((int) (time * 1000.0)) % 1000;
+        
+        auto timecode = String::formatted ("%02d:%02d:%02d.%03d",
+                                           hours,
+                                           minutes,
+                                           seconds,
+                                           millis);
+        
+        auto description = getMidiMessageDescription (message);
+        
+        String midiMessageString (timecode + "  -  " + description + " (" + source + ")"); // [7]
+        logMessage (midiMessageString);
+    }
     //==============================================================================
     void paint (Graphics& g) override;
     void resized() override;
-
-private:
-    waveForms wave; ///call to wave form class in waveforms.h
     
-    //dsp::Oscillator<float> wave2; //created this instance to get the ball rolling on calling juce classes from documentation
-	IIRFilter lp1;
+private:
+    
+    static String getMidiMessageDescription (const MidiMessage& m)
+    {
+        if (m.isNoteOn())           return "Note on "          + MidiMessage::getMidiNoteName (m.getNoteNumber(), true, true, 3);
+        if (m.isNoteOff())          return "Note off "         + MidiMessage::getMidiNoteName (m.getNoteNumber(), true, true, 3);
+        if (m.isProgramChange())    return "Program change "   + String (m.getProgramChangeNumber());
+        if (m.isPitchWheel())       return "Pitch wheel "      + String (m.getPitchWheelValue());
+        if (m.isAftertouch())       return "After touch "      + MidiMessage::getMidiNoteName (m.getNoteNumber(), true, true, 3) +  ": " + String (m.getAfterTouchValue());
+        if (m.isChannelPressure())  return "Channel pressure " + String (m.getChannelPressureValue());
+        if (m.isAllNotesOff())      return "All notes off";
+        if (m.isAllSoundOff())      return "All sound off";
+        if (m.isMetaEvent())        return "Meta event";
+        
+        if (m.isController())
+        {
+            String name (MidiMessage::getControllerName (m.getControllerNumber()));
+            
+            if (name.isEmpty())
+                name = "[" + String (m.getControllerNumber()) + "]";
+            
+            return "Controller " + name + ": " + String (m.getControllerValue());
+        }
+        
+        return String::toHexString (m.getRawData(), m.getRawDataSize());
+    }
+
+    void logMessage (const String& m)
+    {
+        midiMessagesBox.moveCaretToEnd();
+        midiMessagesBox.insertTextAtCaret (m + newLine);
+    }
+    
+     
+	dsp::Oscillator<float> wave;
+    dsp::Oscillator<float> wave2; //created this instance to get the ball rolling on calling juce classes from documentation
+	dsp::Oscillator<float> wave3;
+	dsp::LadderFilter<float> lp1;
+	dsp::Reverb rv6;
+	dsp::Gain<float> lvl;
+	std::vector<MidiMessage> notes;
+	
+	//midi classes
+    
+    
+    AudioDeviceManager deviceManager;           // [1]
+    ComboBox midiInputList;                     // [2]
+    Label midiInputListLabel;
+    int lastInputIndex = 0;                     // [3]
+    bool isAddingFromMidiInput = false;         // [4]
+    
+    MidiKeyboardState keyboardState;            // [5]
+    MidiKeyboardComponent keyboardComponent;    // [6]
+    
+    TextEditor midiMessagesBox;
+    double startTime;
+
+
+    
+
+    
 	
     //i had to manually add this dsp module from the modules section to the left on the projucer here is documentation https://docs.juce.com/master/juce__Oscillator_8h.html
     
@@ -62,18 +197,25 @@ private:
     // Your private member variables go here...
 
 	
-	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
-//=======================members used for gui implementation===========================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+		//=======================members used for gui implementation===========================
 	bool loading;//flag variable we can use to determine whether we're starting with new audio settings or loading from saved presets
-	Slider dial1;
-	Slider dial2;
-	Slider frequencySlider;
-	Label frequencyLabel;
-	Label dial1Label;
-	Label dial2Label;
-	Label durationLabel;
-//=====================================================================================
+	
+	
+	
+	
+	
+	//Filter Sliders and Labels
+	Slider resonanceSlider;
+	Label resonanceLabel;
+	Slider cutoffFrequencySlider;
+	Label cutfrequencyLabel;
+	Slider driveSlider;
+	Label driveLabel;
+	TextButton filterButton;
+	Label filterLabel;
+
+	bool filterType = false; // false = low pass, true = high pass
+	bool noteOn = false;
+	//=====================================================================================
 };
-
-
-
